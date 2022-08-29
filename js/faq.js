f94a458d4ccd9aec047ba55f958a9bc9028f42cb
Helen Titlyanova <elena.titlyanova@gmail.com>
2022-08-29 14:10:14 +0000

    fornat code

--- js/faq.js
+++ js/faq.js
@@ -1,19 +1,19 @@
 {
-const boxes = Array.from(document.querySelectorAll(".box")); 
+  const boxes = Array.from(document.querySelectorAll(".box"));
 
-boxes.forEach((box) => {
-  box.addEventListener("click", boxHandler); 
-});
+  boxes.forEach((box) => {
+    box.addEventListener("click", boxHandler);
+  });
 
-function boxHandler(e) {
-  e.preventDefault();
-  let currentBox = e.target.closest(".box"); 
-  let currentContent = e.target.nextElementSibling;
-  currentBox.classList.toggle("active"); 
-  if (currentBox.classList.contains("active")) { 
-    currentContent.style.maxHeight = currentContent.scrollHeight + "px"; 
-  } else { // в противном случае
-    currentContent.style.maxHeight = 0; 
+  function boxHandler(e) {
+    e.preventDefault();
+    let currentBox = e.target.closest(".box");
+    let currentContent = e.target.nextElementSibling;
+    currentBox.classList.toggle("active");
+    if (currentBox.classList.contains("active")) {
+      currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+    } else { // в противном случае
+      currentContent.style.maxHeight = 0;
+    }
   }
-}
 }
