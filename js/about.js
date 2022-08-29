6a79f74f129497164589903c480bae8a91e1ac60
TarasProject <itgittaras@gmail.com>
2022-08-29 01:08:11 +0000

    create dots to slides

--- js/about.js
+++ js/about.js
@@ -6,41 +6,43 @@
         `img/section-about/partner-jrwins.svg`,
         `img/section-about/partner-hallmark.svg`,       
       ];
-    
-      let currentSlideIdx = 0;
-    
+     
+      let currentSlideIdx = 0;    
       function renderCarousel() {
         const slideContainer = document.querySelector('.about__info-slider-imgs');
-        //slideContainer.innerHTML = slides[currentSlideIdx];
-        slideContainer.innerHTML =
-          `<a href="${slides[currentSlideIdx]}"><img width="100"
-            src="${slides[currentSlideIdx]}" alt="slide about us" /></a>`
-    
         
+        
+        slideContainer.innerHTML =
+          `<a href="${slides[currentSlideIdx]}"><img src="${slides[currentSlideIdx]}" alt="slide about us"/></a>`        
+          
         if (window.innerWidth > 768) {
           const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-          slideContainer.innerHTML += `<a href="${slides[secondSlideIdx]}"><img width="100"
-            src="${slides[secondSlideIdx]}" alt="slide about us" /></a>`;
+          slideContainer.innerHTML += `<a href="${slides[secondSlideIdx]}"><img src="${slides[secondSlideIdx]}" alt="slide about us"/></a>`;
           if (window.innerWidth > 990) {
             const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-            slideContainer.innerHTML += `<a href="${slides[thirdSlideIdx]}"><img width="100"
-            src="${slides[thirdSlideIdx]}" alt="slide about us" /></a>`;
+            slideContainer.innerHTML += `<a href="${slides[thirdSlideIdx]}"><img src="${slides[thirdSlideIdx]}" alt="slide about us"/></a>`;
 
             const fourthSlideIdx = thirdSlideIdx + 1 >= slides.length ? 0 : thirdSlideIdx + 1;
-            slideContainer.innerHTML += `<a href="${slides[fourthSlideIdx]}"><img width="100"
-            src="${slides[fourthSlideIdx]}" alt="slide about us" /></a>`;
+            slideContainer.innerHTML += `<a href="${slides[fourthSlideIdx]}"><img src="${slides[fourthSlideIdx]}" alt="slide about us"/></a>`;
           }
-        }    
-    
-      }    
-    
+        }
+      }
       function next() {
         currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
         renderCarousel();
       }
+
+      function addDotsToSlides() {
+        const slideDotsContainer = document.querySelector('.about__slider-dots');
+        for( let i = 0; i < slides.length; i++) {
+          slideDotsContainer.innerHTML +=
+          `<a href="#"><span class="slider-dot"></span></a>`;          
+        }      
+      }
+      
+      addDotsToSlides();
+
       setInterval(next, 3000);
       renderCarousel();
-      window.addEventListener('resize', renderCarousel); 
-       
-      
+      window.addEventListener('resize', renderCarousel);
 })();
