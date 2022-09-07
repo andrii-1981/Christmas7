8ec88c0a516758d2ca806ced0109a84993d1fc2c
TarasProject <itgittaras@gmail.com>
2022-09-07 01:22:51 +0000

    modal

--- js/about.js
+++ js/about.js
@@ -12,16 +12,16 @@
   function renderCarousel() {
     const slideContainer = document.querySelector('.about__info-slider-imgs');
     slideContainer.innerHTML =
-      `<a href="${slides[currentSlideIdx]}"><img src="${slides[currentSlideIdx]}" alt="slide about us"/></a>`;    
+      `<a href="${slides[currentSlideIdx]}"><img src="${slides[currentSlideIdx]} class = "trigger" alt="slide about us"/></a>`;    
     if (window.innerWidth > 768) {
       const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-      slideContainer.innerHTML += `<a href="${slides[secondSlideIdx]}"><img src="${slides[secondSlideIdx]}" alt="slide about us"/></a>`;
+      slideContainer.innerHTML += `<a href="${slides[secondSlideIdx]}"><img src="${slides[secondSlideIdx]}" class = "trigger" alt="slide about us"/></a>`;
       if (window.innerWidth > 990) {
         const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-        slideContainer.innerHTML += `<a href="${slides[thirdSlideIdx]}"><img src="${slides[thirdSlideIdx]}" alt="slide about us"/></a>`;
+        slideContainer.innerHTML += `<a href="${slides[thirdSlideIdx]}"><img src="${slides[thirdSlideIdx]}" class = "trigger" alt="slide about us"/></a>`;
 
         const fourthSlideIdx = thirdSlideIdx + 1 >= slides.length ? 0 : thirdSlideIdx + 1;
-        slideContainer.innerHTML += `<a href="${slides[fourthSlideIdx]}"><img src="${slides[fourthSlideIdx]}" alt="slide about us"/></a>`;
+        slideContainer.innerHTML += `<a href="${slides[fourthSlideIdx]}"><img src="${slides[fourthSlideIdx]}" class = "trigger"  alt="slide about us"/></a>`;
       }
     }
   }
@@ -71,6 +71,26 @@
       } 
     }
   }
+
+  const modal = document.querySelector(".modal");
+    const trigger = document.querySelector(".trigger");
+    const closeButton = document.querySelector(".close-button");
+    
+    function toggleModal(ev) {
+        modal.classList.toggle("show-modal");
+        ev.stopPropagation();
+    }
+        
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    modal.addEventListener("click", toggleModal);
+
+
+
+
+
+
+
 
   renderCarousel();
   renderDots();
