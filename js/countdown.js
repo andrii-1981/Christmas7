a684abd192c232c75dddddd4637fb5791a980517
olhafursova <fursova.o@gmail.com>
2022-09-08 12:42:37 +0000

    countdown fixed

--- js/countdown.js
+++ js/countdown.js
@@ -17,7 +17,7 @@
       const currentTime = new Date();
       const diff = newYearTime - currentTime;
   
-      const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+      const d = Math.floor(diff / 1000 / 60 / 60 / 24)-7;
       const h = Math.floor(diff / 1000 / 60 / 60) % 24;
       const m = Math.floor(diff / 1000 / 60) % 60;
       const s = Math.floor(diff / 1000) % 60;
